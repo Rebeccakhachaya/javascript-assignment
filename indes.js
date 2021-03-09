6d4c1abdae29504403a43958cc95d5b6779adb4d
rebeccakhachaya <rebeccakhachaya@gmail.com>
2021-03-09 18:19:54 +0300

    my first work

---
+++ indes.js
@@ -0,0 +1,14 @@
+function fruits(){
+    let fruits=["avocado","mangoes","orange","kiwi"];
+    for (let tunda = 0; tunda<= fruits.length; tunda++) {
+        const element = fruits[tunda];
+        console.log(fruits[tunda])
+        
+    }
+}
+    fruits ()   
+    
+        
+
+        
+
